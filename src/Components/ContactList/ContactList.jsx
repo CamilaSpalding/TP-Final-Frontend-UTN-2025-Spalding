393b5765f07e365ad2abbc4a9d7ab2bd81fb1032
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-11 18:07:33 -0300

    Se creó la función de ContactList

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -1,2 +1,12 @@
 import React, { useContext } from 'react'
 import { ContactsContext } from '../../Contexts/ContactsContext'
+
+function ContactList () {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ContactList
