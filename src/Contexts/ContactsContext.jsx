615939b738aaa3cb01304a70e5a2a8055c8b1c1e
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-11 11:34:53 -0300

    se creó el estado para renderizar los contactos en la pantalla de contactos/inicio

--- src/Contexts/ContactsContext.jsx
+++ src/Contexts/ContactsContext.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import { getContactList } from '../service/contactService'
 
-function ContactsContext () {
-  return (
-    <div>ContactsContext</div>
-  )
+export const ContactsContext = createContext({
+    contacts: [],
+    isLoadingContacts: true
+})
+
+const ContactsContextProvider = ({ children }) => {
+    const [contacts, setContacts] = useState(
+        []
+    )
+
+    const [isLoadingContacts, setIsloadingContacts] = useState(true)
+
+    setTimeout(
+        () => {
+            const contact_list = getContactList()
+            setContacts(contact_list)
+            setIsloadingContacts(false)
+        },
+        1000
+    )
+
+    return (
+        <ContactsContaxt.Provider
+            value={
+                {
+                    contacts: contacts,
+                    isLoadingContacts: isLoadingContacts
+                }
+            }
+        >
+            {children}
+        </ContactsContaxt.Provider>
+    )
 }
 
-export default ContactsContext
+export default ContactsContextProvider
