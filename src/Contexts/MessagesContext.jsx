36d2b8a809c7cebc3ac8efa0498c267b255a9f22
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-11 12:39:11 -0300

    se creó la función de agregar mensajes nuevos en el archivo MessagesContext.jsx

--- src/Contexts/MessagesContext.jsx
+++ src/Contexts/MessagesContext.jsx
@@ -6,7 +6,8 @@ export const MessagesContext = createContext(
         messages: [],
         isMessagesLoading: true,
         loadMessages: (contact_id) => {},
-                
+        addNewMessage: (text) => {}
+
     }
 )
 
@@ -30,6 +31,20 @@ const MessagesContextProvider = ({ children }) => {
         const messages = getMessagesByContactId(contact_id)
         setMessages(messages)
     }
+
+    const addNewMessage = (text) => {
+        const newMessage = {
+            id: messages.length + 1, // Rever, buscar una forma de generar IDs que no dependan del orden ni cantidad de mensajes. Que siempre sea único.
+            sender: 'Yo',
+            sent_time: '21:05', // Rever, buscar la forma de que cargue el horario REAL de cuando se envía el mensaje.
+            text: text,
+            status: 'no-recibido'
+        }
+
+        const cloneMessages = [...messages]
+        cloneMessages.push(newMessage)
+        setMessages(cloneMessages)
+    }
     
     return (
         <MessagesContext.Provider
@@ -37,7 +52,8 @@ const MessagesContextProvider = ({ children }) => {
                 {
                     messages: messages,
                     isMessagesLoading: isMessagesLoading,
-                    loadMessages: loadMessages
+                    loadMessages: loadMessages,
+                    addNewMessage: addNewMessage
                 }
             }
         >
