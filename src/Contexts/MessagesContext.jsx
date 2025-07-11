7527006f7ca96a7b21289120cf063a57ec166db8
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-11 13:01:31 -0300

    Se creó la función para eliminar mensajes en el archivo MessagesContext.jsx

--- src/Contexts/MessagesContext.jsx
+++ src/Contexts/MessagesContext.jsx
@@ -6,7 +6,8 @@ export const MessagesContext = createContext(
         messages: [],
         isMessagesLoading: true,
         loadMessages: (contact_id) => {},
-        addNewMessage: (text) => {}
+        addNewMessage: (text) => {},
+        handleDeleteMessage: (message_id) => {}
 
     }
 )
@@ -36,7 +37,7 @@ const MessagesContextProvider = ({ children }) => {
         const newMessage = {
             id: messages.length + 1, // Rever, buscar una forma de generar IDs que no dependan del orden ni cantidad de mensajes. Que siempre sea único.
             sender: 'Yo',
-            sent_time: '21:05',
+            sent_time: '21:05', // Rever, buscar una forma de que cargue la fecha real de cuando se envía un mensaje nuevo.
             text: text,
             status: 'no-recibido'
         }
@@ -44,6 +45,16 @@ const MessagesContextProvider = ({ children }) => {
         const cloneMessages = [...messages]
         cloneMessages.push(newMessage)
         setMessages(cloneMessages)
+    }
+
+    const handleDeleteMessage = (message_id) => {
+        const updatedMessageList = []
+        for (const message of messages) {
+            if (message.id !== message_id) {
+                updatedMessageList.push(message)
+            }
+        }
+        setMessages(updatedMessageList)
     }
     
     return (
@@ -53,7 +64,8 @@ const MessagesContextProvider = ({ children }) => {
                     messages: messages,
                     isMessagesLoading: isMessagesLoading,
                     loadMessages: loadMessages,
-                    addNewMessage: addNewMessage
+                    addNewMessage: addNewMessage,
+                    handleDeleteMessage: (message_id) => {}
                 }
             }
         >
