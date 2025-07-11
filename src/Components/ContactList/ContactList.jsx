c3680c312d2798d8eb37d873a38e7c57ca3310b7
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-11 18:10:28 -0300

    Se creó el componente ContactItem y se lo importó dentro del componente ContactList

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -1,10 +1,17 @@
 import React, { useContext } from 'react'
 import { ContactsContext } from '../../Contexts/ContactsContext'
+import ContactItem from 
 
 function ContactList () {
     return (
         <div>
-
+            {
+                contacts.map(
+                    (contact) => {
+                        return 
+                    }
+                )
+            }
         </div>
     )
 }
