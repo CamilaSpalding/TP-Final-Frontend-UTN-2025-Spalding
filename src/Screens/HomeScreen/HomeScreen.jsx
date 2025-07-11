f586d9013fe569a8900f9e4ccaf68d631abfae39
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-11 19:19:51 -0300

    Se crearon algunos de los componentes de la página

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,6 +1,6 @@
 // Esta página va a ser la ContactScreen
 import React from 'react'
-import ContactList from
+import ContactList from '../../Components/ContactList/ContactList'
 
 function HomeScreen () {
     return (
