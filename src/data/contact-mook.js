f5accace94e4361aea67a2b275c53f8daf40c0cc
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-11 10:50:35 -0300

    se creó la variable mook_data con la estructura de datos para simular un servidor. Falta cargar los valores.

---
+++ src/data/contact-mook.js
@@ -0,0 +1,60 @@
+// Este archivo solo contiene los datos de los contactos, simula ser un servidor.
+
+const mook_data = {
+    constacts: [
+        {
+            id:,
+            name:,
+            last_time_connecte:,
+            profile_pic:,
+            last message: {
+                id:,
+                text:                
+            },
+            unread_messages:,
+            messages: [
+                {
+                    emisor:,
+                    hora:,
+                    id:,
+                    texto:,
+                    status:
+                },
+                {
+                    emisor:,
+                    hora:,
+                    id:,
+                    texto:,
+                    status:
+                },
+            ]
+        },
+        {
+            id:,
+            name:,
+            last_time_connecte:,
+            profile_pic:,
+            last message: {
+                id:,
+                text:                
+            },
+            unread_messages:,
+            messages: [
+                {
+                    emisor:,
+                    hora:,
+                    id:,
+                    texto:,
+                    status:
+                },
+                {
+                    emisor:,
+                    hora:,
+                    id:,
+                    texto:,
+                    status:
+                },
+            ]
+        },
+    ]}
+}
