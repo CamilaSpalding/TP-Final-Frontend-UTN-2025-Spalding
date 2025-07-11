e440f6d18810e6428e017777593a4aac99609558
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-11 14:38:48 -0300

    se eliminó la carpeta ContactScreen ya que esa pantalla será la HomeScreen de ahora en más

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,9 +1,10 @@
+// Esta página va a ser la ContactScreen
 import React from 'react'
 
 function HomeScreen () {
     return (
         <div>
-
+            <ContactsList /> 
         </div>
     )
 }
