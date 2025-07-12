31f7d50fc137bbf0674006ea0db7f354cf748234
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-12 12:40:52 -0300

    Se le agregó contenido al componente de ChatItem. Aclaración: aún falta mejorarlo.

--- src/Components/ChatItem/ChatItem.jsx
+++ src/Components/ChatItem/ChatItem.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router'
 
-function ContactItem () {
+function ChatItem({ id, name, last_time_connected, profile_pic, last_message, unread_message }) {
     return (
-        <div>
-            
-        </div>
+        <Link to={`/contacts/${id}/chat`}>
+            <img src={profile_pic} alt={`${name}'s profile image`} />
+            <h3>{name}</h3>
+            <div>
+                {last_message.text}
+            </div>
+            <span>{unread_message}</span>
+            <span>Última conección: {last_time_connected}</span>
+        </Link>
     )
 }
 
-export default ContactItem
+export default ChatItem
