450c1f0795e96cdbb2a5f4c877f6eacc471c7161
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-16 12:57:34 -0300

    Se creó la estructura del componente Sidebar, aún queda importar los demás iconos y llamarlos en sus respectivos botones.

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Sidebar.css'
+
+/* Iconos en línea */
+import ChatsLineIcon from '../../assets/icons/chats-line.svg'
+
+/* Iconos sólidos */
+import ChatsSolidIcon from '../../assets/icons/chats-solid.svg'
 
 function Sidebar() {
+
+    const [ hovered, setHovered ] = useState({
+        chats: false,
+        status: false,
+        channels: false,
+        communities: false,
+        settings: false
+    })
+
     return (
         <aside className='sidebar'>
-            <nav className='sidebar-nav'>
-                <div className='chat-action-buttons'>
+            <nav className='sidebar__nav'>
+                <div className='sidebar__main-actions'>
+                    <button className='sidebar__button' aria-label='Chats' onMouseEnter={() => setHovered(true)} onMouseLeave={() => setIsHoveredChats(false)}>
+                        {hovered.chats ? <ChatsSolidIcon className="sidebar__btn-icon"/> : <ChatsLineIcon className="sidebar__btn-icon"/>}
+                    </button>
 
+                    <button className='sidebar__button' aria-label='Estados'>
+                        {/* icono de Estados */}
+                    </button>
+
+                    <button className='sidebar__button' aria-label='Canales'>
+                        {/* icono de Canales */}
+                    </button>
+
+                    <button className='sidebar__button' aria-label='Comunidades'>
+                        {/* icono de Comunidades */}
+                    </button>
                 </div>
 
-                <div className='profile-action-buttons'>
+                <div className='sidebar__user-actions'>
+                    <button className='sidebar__button' aria-label='Ajustes'>
+                        {/* icono de Ajustes */}
+                    </button>
+
+                    <button className='sidebar__button sidebar__profile-btn' aria-label='Perfil'>
+                        <img src="/src/assets/images/user-profile.jpg" alt="User profile" className='sidebar__profile-image' />
+                    </button>
 
                 </div>
 
