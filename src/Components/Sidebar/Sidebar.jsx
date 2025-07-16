baf9213079c47711b2ed55503615ea3dbc32f28a
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-16 17:21:21 -0300

    Se ajustaron los estilos del sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -32,55 +32,55 @@ function Sidebar() {
             <nav className='sidebar__nav'>
                 <div className='sidebar__main-actions'>
                     <button
-                        className='sidebar__button'
+                        className='sidebar__button sidebar__small-icon-btn'
                         aria-label='Chats'
                         onMouseEnter={() => setHovered(previousState => ({ ...previousState, chats: true }))}
                         onMouseLeave={() => setHovered(previousState => ({ ...previousState, chats: false }))}
                     >
                         { hovered.chats 
-                            ? (<ChatsSolidIcon className="sidebar__btn-icon"/>) 
-                            : (<ChatsLineIcon className="sidebar__btn-icon"/>)
+                            ? (<ChatsSolidIcon className="sidebar__btn-icon sidebar__small-icon"/>) 
+                            : (<ChatsLineIcon className="sidebar__btn-icon sidebar__small-icon"/>)
                         }
                     </button>
 
                     <button 
-                        className='sidebar__button'
+                        className='sidebar__button sidebar__small-icon-btn'
                         aria-label='Estados'
                         onMouseEnter={() => setHovered(previousState => ({ ...previousState, status: true }))}
                         onMouseLeave={() => setHovered(previousState => ({ ...previousState, status: false }))}
                     >
                         { hovered.status
-                            ? (<StatusSolidIcon className="sidebar__btn-icon"/>) 
-                            : (<StatusLineIcon className="sidebar__btn-icon"/>)
+                            ? (<StatusSolidIcon className="sidebar__btn-icon sidebar__small-icon"/>) 
+                            : (<StatusLineIcon className="sidebar__btn-icon sidebar__small-icon"/>)
                         }
                     </button>
 
                     <button 
-                        className='sidebar__button' 
+                        className='sidebar__button sidebar__small-icon-btn' 
                         aria-label='Canales'
                         onMouseEnter={() => setHovered(previousState => ({ ...previousState, channels: true }))}
                         onMouseLeave={() => setHovered(previousState => ({ ...previousState, channels: false }))}
                     >
                         { hovered.channels
-                            ? (<ChannelsSolidIcon className="sidebar__btn-icon"/>) 
-                            : (<ChannelsLineIcon className="sidebar__btn-icon"/>)
+                            ? (<ChannelsSolidIcon className="sidebar__btn-icon sidebar__small-icon"/>) 
+                            : (<ChannelsLineIcon className="sidebar__btn-icon sidebar__small-icon"/>)
                         }
                     </button>
 
                     <button 
-                        className='sidebar__button' 
+                        className='sidebar__button sidebar__large-icon-btn' 
                         aria-label='Comunidades'
                         onMouseEnter={() => setHovered(previousState => ({ ...previousState, communities: true }))}
                         onMouseLeave={() => setHovered(previousState => ({ ...previousState, communities: false }))}
                     >
                         { hovered.communities
-                            ? (<CommunitiesSolidIcon className="sidebar__btn-icon large-icon"/>) 
-                            : (<CommunitiesLineIcon className="sidebar__btn-icon large-icon"/>)
+                            ? (<CommunitiesSolidIcon className="sidebar__btn-icon sidebar__large-icon"/>) 
+                            : (<CommunitiesLineIcon className="sidebar__btn-icon sidebar__large-icon"/>)
                         }
                     </button>
                 </div>
 
-                <div className='sidebar__user-actions'>
+                <div className='sidebar__user-actions sidebar__small-icon-btn'>
                     <button 
                         className='sidebar__button' 
                         aria-label='Ajustes'
@@ -88,8 +88,8 @@ function Sidebar() {
                         onMouseLeave={() => setHovered(previousState => ({ ...previousState, settings: false }))}
                     >
                         { hovered.settings
-                            ? (<SettingsSolidIcon className="sidebar__btn-icon"/>) 
-                            : (<SettingsLineIcon className="sidebar__btn-icon"/>)
+                            ? (<SettingsSolidIcon className="sidebar__btn-icon sidebar__small-icon"/>) 
+                            : (<SettingsLineIcon className="sidebar__btn-icon sidebar__small-icon"/>)
                         }
                     </button>
 
