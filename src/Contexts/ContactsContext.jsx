14b913d2c503a121f09102796d50029b3d5db49b
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-17 17:30:48 -0300

    Se hace una prueba para ver si se soluciona el error de mapeo

--- src/Contexts/ContactsContext.jsx
+++ src/Contexts/ContactsContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { getContactList } from '../services/contactService'
 
 export const ContactsContext = createContext({
@@ -11,14 +11,26 @@ const ContactsContextProvider = ({ children }) => {
     const [contacts, setContacts] = useState([])
     const [isLoadingContacts, setIsloadingContacts] = useState(true)
 
-    setTimeout(
+    /* setTimeout(
         () => {
             const contactList = getContactList()
             setContacts(contactList)
             setIsloadingContacts(false)
         },
         1000
-    )
+    ) */
+
+    useEffect(() => {
+        const fetchContacts = () => {
+            const contactsList = getContactsList()
+            setContacts(contactsList)
+            setIsloadingContacts(false)
+
+        }
+        const timeout = setTimeout(fetchContacts, 1000)
+
+        return () => clearTimeout(timeout)
+    }, [])
 
     return (
         <ContactsContext.Provider
