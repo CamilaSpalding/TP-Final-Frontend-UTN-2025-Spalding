127a74d2a1e41449e3592bab08c4e29ace91bf7a
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-17 13:21:52 -0300

    Se creó la estructura del AppDownloadPanel y se le están dando los estilos

--- src/Components/AppDownloadPanel/AppDownloadPanel.jsx
+++ src/Components/AppDownloadPanel/AppDownloadPanel.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import './AppDownloadPanel.css'
 
+/* Imagen */
+import downloadAppImage from '../../assets/images/whatsapp-videocall.png'
+
+/* Icono */
+import LockIcon from '../../assets/icons/lock.svg?react'
+
 function AppDownloadPanel() {
     return (
-        <aside className='app-download-panel'>
-            <div>
-                App Download
+        <section className='app-download-panel'>
+            <div className='app-download__content'>
+                <img src={downloadAppImage} alt="Imagen de videollamada en whatsapp" className='app-download__image' />
+
+                <h1 className='app-download__title'>Descarga WhatsApp para Mac</h1>
+
+                <p className='app-downloadd__text'>Descarga la aplicación para Mac y haz llamadas con mayor rapidez.</p>
+
+                <button className='app-download__button'>Descargar de App Store</button>
             </div>
-        </aside>
+
+            <footer className='app-download__footer'>
+                <LockIcon className='app-download__icon' />
+
+                <p className='app-download__text'>Tus mensajes personales están cifrados de extremo a extremo.</p>
+            </footer>
+        </section>
     )
 }
 
