fe6db6e53eeaab924333e092ff9f847db81058a6
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-17 17:12:46 -0300

    Se creó la estructura del componente ContactsPanel

--- src/Components/ContactsPanel/ContactsPanel.jsx
+++ src/Components/ContactsPanel/ContactsPanel.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import ContactsPanelNavbar from '../ContactsPanelNavbar/ContactsPanelNavbar'
+import ContactsPanelSearcher from '../ContactsPanelSearcher/ContactsPanelSearcher'
+import ChatFilter from '../ChatFilter/ChatFilter'
+import AppNotification from '../AppNotification/AppNotification'
+import ArchivedMessages from '../ArchivedMessages/ArchivedMessages'
+import ChatsList from '../ChatsList/ChatsList'
 
-function ContactsPanel() {
+function ContactsPanel () {
     return (
-        <div>ContactsPanel</div>
+        <section className='contacts-panel'>
+            <div className='contacts-panel__header'>
+                <ContactsPanelNavbar />
+                <ContactsPanelSearcher />
+                <ChatFilter />
+                <AppNotification />
+            </div>
+
+            <div className='contacts-panel__main'>
+                <ArchivedMessages />
+                <ChatsList />
+            </div>
+        </section>
     )
 }
 
