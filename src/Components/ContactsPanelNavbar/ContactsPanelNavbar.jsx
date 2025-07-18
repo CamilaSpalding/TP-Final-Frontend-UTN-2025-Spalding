db4c204f0c52c5ec0684a9fc005a2cfcde3cf464
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-18 16:32:26 -0300

    Se le asignaron algunos estilos al ContactsPanelNavbar

--- src/Components/ContactsPanelNavbar/ContactsPanelNavbar.jsx
+++ src/Components/ContactsPanelNavbar/ContactsPanelNavbar.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import './ContactsPanelNavbar.css'
+
+/* Logo */
+import WhatsappLogo from '../../assets/logo/whatsapp-wordmark.svg'
+
+/* Iconos */
+import NewChatIcon from '../../assets/icons/new-chat.svg'
+import HamburgerMenuIcon from '../../assets/icons/hamburger-menu.svg'
 
 function ContactsPanelNavbar() {
     return (
-        <div>ContactsPanelNavbar</div>
+        <header className='contacts-panel-navbar'>
+            <div className='contacts-panel-navbar__logo-container'>
+                <WhatsappLogo className='contacts-panel-navbar__wa-logo' />
+            </div>
+
+            <nav className='contacts-panel-navbar__nav'>
+                <button
+                    className='contacts-panel-navbar__button'
+                    aria-label='Nuevo chat'
+                >
+                    <NewChatIcon className='contacts-panel-navbar__icon' />
+                </button>
+
+                <button
+                    className='contacts-panel-navbar__button'
+                    aria-label='Menú'
+                >
+                    <HamburgerMenuIcon className='contacts-panel-navbar__icon' />
+                </button>
+            </nav>
+        </header>
     )
 }
 
