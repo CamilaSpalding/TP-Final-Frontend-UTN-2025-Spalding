1b3b4f58e5a4ae179aee0b7da20db0f649e95347
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-18 15:41:29 -0300

    Se le dió estilos y se armó el responsive del componente ContactsPanel

--- src/Components/ContactsPanel/ContactsPanel.jsx
+++ src/Components/ContactsPanel/ContactsPanel.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import './ContactsPanel.css'
+
+/* Componentes */
 import ContactsPanelNavbar from '../ContactsPanelNavbar/ContactsPanelNavbar'
 import ContactsPanelSearcher from '../ContactsPanelSearcher/ContactsPanelSearcher'
 import ChatFilter from '../ChatFilter/ChatFilter'
@@ -6,19 +9,26 @@ import AppNotification from '../AppNotification/AppNotification'
 import ArchivedMessages from '../ArchivedMessages/ArchivedMessages'
 import ChatsList from '../ChatsList/ChatsList'
 
+/* Icono */
+import LockIcon from '../../assets/icons/lock.svg?react'
+
 function ContactsPanel () {
     return (
         <section className='contacts-panel'>
-            <div className='contacts-panel__header'>
+            <div className='contacts-panel__nav'>
                 <ContactsPanelNavbar />
                 <ContactsPanelSearcher />
                 <ChatFilter />
                 <AppNotification />
             </div>
 
-            <div className='contacts-panel__main'>
+            <div className='contacts-panel__chats'>
                 <ArchivedMessages />
                 <ChatsList />
+
+                <div className='contacts-panel__footer-note'>
+                    <p className='contacts-panel__footer-text'><span className='contacts-panel__footer-icon-container'><LockIcon className="contacts-panel__footer-icon" /></span> Tus mensajes personales están cifrados de extremo a extremo</p> {/* Acá faltaría un lin en las palabras "cifrados de extremo a extremo." */}
+                </div>
             </div>
         </section>
     )
