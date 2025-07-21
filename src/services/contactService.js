115fbd4a3054bb54d60837a6b3652d919a5fc55c
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-21 20:41:06 -0300

    Se ajustó el contactService a la nueva estructura del mook_data

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,15 +1,21 @@
 import mook_data from "../data/contact-mook.js"
 
+export const getCurrentUser = () => {
+    return mook_data.current_user
+}
+
 export const getContactsList = () => {
     return mook_data.contacts
 }
 
+export const getGroupsList = () => {
+    return mook_data.groups
+}
+
 export const getContactById = (contact_id) => {
-    for(const contact of mook_data.contacts){
-        if(Number(contact.id) === Number(contact_id)){
-            return contact
-        }
-    }
-    
-    return null
+    return mook_data.contacts.find(contact => contact.id === contact_id) || null
+}
+
+export const getGroupById = (group_id) => {
+    return mook_data.groups.find(group => group.id === group_id) || null
 }
