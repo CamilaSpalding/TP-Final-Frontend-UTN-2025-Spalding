61b28957f4736171101c373cf1c4ba37613e149b
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-23 13:45:53 -0300

    Se limpió el código del ContactsContext

--- src/Contexts/ContactsContext.jsx
+++ src/Contexts/ContactsContext.jsx
@@ -13,7 +13,6 @@ export const ContactsContext = createContext({
         groups: [],
         messages: []
     },
-    /* filteredContacts: [], */
     searchContacts: (searchText) => {}
 })
 
@@ -24,7 +23,6 @@ const ContactsContextProvider = ({ children }) => {
     const [ groups, setGroups ] = useState([])
     const [ isLoadingContacts, setIsLoadingContacts ] = useState(true)
     const [ isSearching, setIsSearching ] = useState(false)
-    /* const [ filteredContacts, setFilteredContacts ] = useState([]) */
 
     const [ filteredResults, setFilteredResults ] = useState({
         chats: [],
@@ -33,26 +31,7 @@ const ContactsContextProvider = ({ children }) => {
         messages: []
     })
 
-    /* setTimeout(
-        () => {
-            const contactList = getContactList()
-            setContacts(contactList)
-            setIsloadingContacts(false)
-        },
-        1000
-    ) */
-
     useEffect(() => {
-
-        /* const fetchContacts = () => {
-            const contactsList = getContactsList()
-            setContacts(contactsList)
-            setIsloadingContacts(false)
-            setFilteredContacts(contactsList)
-        }
-        const timeout = setTimeout(fetchContacts, 1000)
-        return () => clearTimeout(timeout) */
-
         const timeout = setTimeout(() => {
             const fetchedCurrentUser = getCurrentUser()
             const fetchedContacts = getContactsList()
@@ -84,14 +63,6 @@ const ContactsContextProvider = ({ children }) => {
             /* Para la búsqueda de CHATS iniciados que coincida el nombre del contacto con el valor ingresado */
             if (nameMatches && hasMessages) {
                 chats.push(contact)
-
-                /* ANTERIOR: Para que la búsqueda muestre los GRUPOS en común del contacto CON chat inicializado que coincida con el valor ingresado */
-                /* if (contact.groups && contact.groups.length > 0) {
-                    const matchedGroups = contact.groups.filter(group =>
-                        group.name.toLowerCase().includes(lowered)
-                    )
-                    groups.push(...matchedGroups)
-                } */
             }
 
             /* Para la búsqueda de CONTACTOS SIN chats inicializados que coincidan con el valor ingresado */
@@ -101,18 +72,6 @@ const ContactsContextProvider = ({ children }) => {
 
             /* Para la búsqueda de MENSAJES que coincidan con el valor ingresado. */
             if (hasMessages) {
-                /* const matchedMessages = contact.messages.filter(msg =>
-                    msg.text.toLowerCase().includes(lowered)
-                ).map(msg => ({
-                    ...msg,
-                    contact_id: contact.id,
-                    contact_name: contact.name,
-                    profile_pic: contact.profile_pic,
-                    sent_time: msg.sent_time
-                }))
-
-                messages.push(...matchedMessages) */
-
                 const matched = contact.messages.filter(msg =>
                     msg.text.toLowerCase().includes(lowered)
                 ).map(msg => ({
@@ -151,21 +110,7 @@ const ContactsContextProvider = ({ children }) => {
             }
         })
 
-        /* messages.sort((a, b) => {
-            const timeA = a.sent_time
-            const timeB = b.sent_time
-
-            return timeB.localeCompare(timeA)
-        }) */
-
         matchedMessages.sort((a, b) => b.sent_time.localeCompare(a.sent_time))
-
-        /* setFilteredResults({
-            chats: chats,
-            contacts: contactsOnly,
-            groups: groups,
-            messages: messages
-        }) */
         
         setFilteredResults({
             chats: chats,
@@ -186,7 +131,6 @@ const ContactsContextProvider = ({ children }) => {
                     groups: groups,
                     isLoadingContacts: isLoadingContacts,
                     isSearching: isSearching,
-                    /* filteredContacts: filteredContacts, */
                     filteredResults: filteredResults,
                     searchContacts: searchContacts
                 }
