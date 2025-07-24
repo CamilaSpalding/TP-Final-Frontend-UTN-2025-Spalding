22f8a4c15c40dc05c2e248d83b0aa13ae5617405
CamilaSpalding <camilaspaldingdg@gmail.com>
2025-07-24 19:22:13 -0300

    Se ajustó la logica del componente Message

--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -1,15 +1,14 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { MessagesContext } from '../../Contexts/MessagesContext'
 import './Message.css'
 
 // Iconos
-/* import SentCheckIcon from '../../assets/icons/sent-check.svg?react'
-import ReceivedCheckIcon from '../../assets/icons/received-check.svg?react'
-import SeenCheckIcon from '../../assets/icons/seen-check.svg?react'
-import SendingClockIcon from '../../assets/icons/sending-clock.svg?react'
+import SendingClockIcon from '../../assets/icons/msg-sending-clock.svg?react'
+import SingleCheckIcon from '../../assets/icons/msg-single-check.svg?react'
+import DoubleCheckIcon from '../../assets/icons/msg-double-check.svg?react'
 import ChevronDownIcon from '../../assets/icons/chevron-down.svg?react'
 import EditIcon from '../../assets/icons/pencil.svg?react'
-import DeleteIcon from '../../assets/icons/trash.svg?react' */
+import DeleteIcon from '../../assets/icons/trash.svg?react'
 
 
 // FORMA VISTA EN CLASE
@@ -39,12 +38,12 @@ import DeleteIcon from '../../assets/icons/trash.svg?react' */
 export default Message */
 
 // MI MANERA
-function Message ({id, sender, sent_time, text, status}) {
+function Message ({id, sender_id, sent_time, text, status, deleted}) {
+    
     const { handleDeleteMessage, handleSoftDeleteMessage /* handleEditMessage */ } = useContext(MessagesContext)
-
     const [ showOptions, setShowOptions ] = useState(false)
 
-    const myMessage = sender.toLowerCase() === 'yo'
+    const myMessage = sender_id === 0
     
     const messageClass = myMessage
         ? 'chat-dialog my-message'
@@ -53,6 +52,16 @@ function Message ({id, sender, sent_time, text, status}) {
     const chatBubbleTailClass = myMessage
         ? 'chat-bubble-tail tail-right'
         : 'chat-bubble-tail tail-left'
+
+    // Se agrega función para seleccionar el icono según el estado
+    const iconMap = {
+        sending: SendingClockIcon,
+        sent: SingleCheckIcon,
+        received: DoubleCheckIcon,
+        seen: DoubleCheckIcon
+    }
+
+    const StatusIcon = myMessage && status ? iconMap[status] : null
 
     const toggleOptions = () => {
         setShowOptions(previousValue => !previousValue)
@@ -71,71 +80,67 @@ function Message ({id, sender, sent_time, text, status}) {
         }
     }
 
+    // Si el mensaje está eliminado
+    /* if (deleted || text === 'Mensaje eliminado') {
+        return (
+            <div className={`${messageClass} deleted`}>
+                <span className='deleted-text'>{text}</span>
+                <div className={chatBubbleTailClass}></div>
+            </div>
+        )
+    } */
+
     const handleEdit = () => {
         closeOptions()
         console.log(`Editar mensaje con ID: ${id}`) // Todavía tengo que hacerle la lógica para editar el mensaje (Popup con el input para editar el mensaje y volver a enviarlo).
     }
-
-    // Se agrega función para seleccionar el icono según el estado
-    const getStatusIcon = (status) => {
-        switch (status) {
-            case 'sending':
-                return SendingClockIcon
-            case 'sent':
-                return SentCheckIcon
-            case 'received':
-                return ReceivedCheckIcon
-            case 'seen':
-                return SeenCheckIcon
-                default:
-                    return null
-        }
-    }
-
-    const StatusIcon = myMessage ? getStatusIcon(status) : null
 
 
     return (
         <div className={messageClass} onMouseLeave={closeOptions}>
             <div className="message-content">
                 <span className="message-text">
-                {text}
+                    { deleted ? <em>{text}</em> : text}
 
-                <div className="message-meta">
-                    <span className="message-time">{sent_time}</span>
-                    {StatusIcon && (
-                    <StatusIcon
-                        className={`status-icon status-${status}`}
-                        title={`Status: ${status}`}
-                        aria-label={`Message status: ${status}`}
-                    />
-                    )}
-                </div>
+                    <div className="message-meta">
+                        <span className="message-time">{sent_time}</span>
+                        
+                        { StatusIcon && (
+                            <StatusIcon
+                                className={`status-icon ${status === 'seen' ? 'blue' : 'gray'}`}
+                                aria-label={`Estado del mensaje: ${status}`}
+                                title={status}
+                            />
+                        )}
+                    </div>
                 </span>
 
                 {/* Chevron y menú de opciones */}
-                <div className="message-actions-wrapper">
-                <button
-                    className="options-button"
-                    onClick={toggleOptions}
-                    aria-label="Show message options"
-                >
-                    <img src={ChevronDownIcon} alt="More options" />
-                </button>
+                { !deleted && (
+                    <div className="message-actions-wrapper">
+                        <button
+                            className="options-button"
+                            onClick={toggleOptions}
+                            aria-label="Show message options"
+                        >
+                            <ChevronDownIcon />
+                        </button>
 
-                {showOptions && (
-                    <div className="message-options-menu">
-                    <button onClick={handleEdit}>
-                        <img src={EditIcon} alt="Edit" />
-                        <span>Editar</span>
-                    </button>
-                    <button onClick={handleDelete} className="delete-option">
-                        <img src={DeleteIcon} alt="Delete" />
-                        <span>Eliminar</span>
-                    </button>
+                        { showOptions && (
+                            <div className="message-options-menu">
+                                <button onClick={handleEdit}>
+                                    <EditIcon />
+                                    <span>Editar</span>
+                                </button>
+
+                                <button onClick={handleDelete} className="delete-option">
+                                    <DeleteIcon />
+                                    <span>Eliminar</span>
+                                </button>
+                            </div>
+                        )}
                     </div>
                 )}
-                </div>
             </div>
 
             <div className={chatBubbleTailClass}></div>
